9cf3f5580521a913126f8ae1c4366a1ddaa3eec0
Mason Crane <masonecrane@gmail.com>
2015-07-22 15:22:08 -0700

    added fragments and activity

---
+++ app/src/main/java/com/example/skarshon/attendanceapp/fragments/AttendanceListFragment.java
@@ -0,0 +1,31 @@
+package com.example.skarshon.attendanceapp.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.skarshon.attendanceapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AttendanceListFragment extends Fragment {
+
+
+    public AttendanceListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_attendance_list, container, false);
+    }
+
+
+}
